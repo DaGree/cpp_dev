c1ea7467e7d36a04a922cba7fabbccc2f4fb0874
DaGree <mr.dan6886@yandex.ru>
2021-09-28 15:30:32 +0300

    2nd commit

--- CA_1/CA_1.cpp
+++ CA_1/CA_1.cpp
@@ -3,9 +3,11 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!" << endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
